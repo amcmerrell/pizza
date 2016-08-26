cc0e5ec89064c975e47f74c93e03ff199f45c08b
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-26 08:03:51 -0700

    add size and toppings form fields to html and begin UI logic in order to test input variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 // Business Logic
-function Pizza(size, toppings) {
+function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = [];
+  this.price = price;
 }
 
-Pizza.prototype.addToppings = function() {
-  for (var i = 0; i < toppings.length; i++) {
-    this.toppings.push(topping[i])
-  }
-
-}
+// Pizza.prototype.addToppings = function() {
+//   for (var topping = 0; i < toppings.length; topping++) {
+//     this.toppings.push(topping)
+//   }
+// }
 
 // UI Logic
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var size = $("#size").val();
+    
+  });
+});
