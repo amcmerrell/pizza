e52f3ab51c75e42ee6461cf62c3a120828e2d4dd
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-26 09:10:49 -0700

    add working createPrice prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,19 @@ function Pizza(size, toppings, price) {
   this.price = price;
 }
 
+Pizza.prototype.createPrice = function() {
+  if (this.size === "Small") {
+    this.price = 10+(this.toppings.length*2);
+  } else if (this.size === "Medium") {
+    this.price = 12+(this.toppings.length*2);
+  } else if (this.size === "Large") {
+    this.price = 14+(this.toppings.length*2);
+  } else {
+    this.price = 16+(this.toppings.length*2);
+  }
+  return this.price;
+}
+
 // UI Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
@@ -15,6 +28,7 @@ $(document).ready(function() {
     $("input:checkbox[name=toppings]:checked").each(function() {
       newPizza.toppings.push($(this).val());
     });
-    console.log(newPizza);
+    console.log(newPizza.createPrice());
+
   });
 });
