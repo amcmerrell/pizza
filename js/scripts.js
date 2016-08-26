0473e676d0596af5aaa0a438d94c31ca7b0f5a05
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-25 21:05:04 -0700

    link html file to js + css files and add pizza constructor to javascript.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Business Logic
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.addToppings = function() {
+  for (var i = 0; i < toppings.length; i++) {
+    this.toppings.push(topping[i])
+  }
+
+}
+
+// UI Logic
